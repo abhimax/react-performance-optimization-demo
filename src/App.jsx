c778fccac60ea6431e165c70c1deebee2c675a38
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-08-11 23:44:49 +0530

    feat: react memo sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useMemo } from "react";
 import "./App.css";
-import { ItemList } from "./modules/memo/ItemList";
-import { items } from "./modules/memo/data";
+import { ItemList } from "./modules/memo1/ItemList";
+import { items } from "./modules/memo1/data";
+import ItemList2 from "./modules/memo2/ItemList2";
 
 function App() {
   const memorizedItems = useMemo(() => items, [items]);
-  return <ItemList items={memorizedItems} />;
+  return (
+    <>
+      <ItemList items={memorizedItems} />
+      <ItemList2 />
+    </>
+  );
 }
 
 export default App;
